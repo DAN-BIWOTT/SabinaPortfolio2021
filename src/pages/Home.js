b36ac4b1f3990c903b975e1ff8e362690c9228cb
DAN-BIWOTT <dankibiwott254@gmail.com>
2022-02-24 21:56:28 +0300

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Projects from "../components/Projects/Projects";
 import About from "../components/About/About";
 import Contact from "../components/Contact/Contact";
 import Footer from "../components/Footer/Footer";
-import FixSocialIcon from "../components/SocialIcon/FixSocialIcon";
+// import FixSocialIcon from "../components/SocialIcon/FixSocialIcon";
 function Home() {
   return (
     <>
